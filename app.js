9b35a07e0c7abe2d037072a6fe71b9c7f0936ab5
rigorsaif <31529436+rigorsaif@users.noreply.github.com>
2018-11-12 20:24:00 +0000

    cores issue

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ mongoose.connect(DB_URL).then(() => {
 app.get("/", (req, res, next) => {
   res.sendfile(`${__dirname}/views/index.html`);
 });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use("/api", apiRouter);
 
