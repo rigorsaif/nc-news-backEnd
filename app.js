6f4dca82ac2d2440677c779e394c18859af97a66
rigorsaif <31529436+rigorsaif@users.noreply.github.com>
2018-11-13 09:08:26 +0000

    cors solved for post and patch

--- app.js
+++ app.js
@@ -17,11 +17,19 @@ app.get("/", (req, res, next) => {
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Access-Control-Allow-Methods",
+    "GET,PUT,POST,DELETE,PATCH,OPTIONS"
   );
-  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
-  next();
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With"
+  );
+  // allow preflight
+  if (req.method === "OPTIONS") {
+    res.send(200);
+  } else {
+    next();
+  }
 });
 app.use(bodyParser.json());
 app.use("/api", apiRouter);
