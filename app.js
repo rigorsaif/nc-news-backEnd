2a5b8f06c8985d413b0e354d5053b7653a480c6f
rigorsaif <31529436+rigorsaif@users.noreply.github.com>
2018-11-13 08:59:58 +0000

    cors solved for post and patch

--- app.js
+++ app.js
@@ -14,9 +14,13 @@ mongoose.connect(DB_URL).then(() => {
 app.get("/", (req, res, next) => {
   res.sendfile(`${__dirname}/views/index.html`);
 });
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, OPTIONS");
   next();
 });
 app.use(bodyParser.json());
