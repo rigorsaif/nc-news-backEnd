c15639fe740dca13ecd2d7586aa4783560c6b1b8
Mahmoud Hosni <mahmoudmhosni7@gmail.com>
2019-01-27 13:57:32 +0000

    add production vars to heroku

--- utils/logger.js
+++ utils/logger.js
@@ -1,57 +1,33 @@
 const { format, createLogger, transports } = require("winston");
-const {
-  AccessKeyID,
-  SecretAccessKey,
-  username
-} = require("../config/cloudWatchConfig");
 const CloudWatchTransport = require("winston-aws-cloudwatch");
-
-const options = {
-  error: {
-    level: "error",
-    filename: `${__dirname}/../logs/appErrors.log`,
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    colorize: false
-  },
-  debug: {
-    level: "debug",
-    filename: `${__dirname}/../logs/appDebug.log`,
-    handleExceptions: true,
-    json: true,
-    maxsize: 5242880, // 5MB
-    maxFiles: 5,
-    colorize: false
-  },
-  console: {
-    level: "debug",
-    handleExceptions: true,
-    json: false,
-    colorize: true
-  }
-};
-
-const level =
-  process.env.NODE_ENV === "production" ? options.level : options.debug;
+const {
+  logLevel,
+  streamName,
+  accessKeyID,
+  secretAccessKey,
+  groupName,
+  region
+} =
+  process.env.NODE_ENV === "production"
+    ? process.env
+    : require("../config/cloudWatchConfig");
 
 const logger = createLogger({
   transports: [
     new CloudWatchTransport(
       {
-        level: "debug",
-        logGroupName: "minia-news-logs", // REQUIRED
-        logStreamName: "ERR", // REQUIRED
+        level: logLevel,
+        logGroupName: groupName, // REQUIRED
+        logStreamName: streamName, // REQUIRED
         createLogGroup: false,
         createLogStream: true,
         submissionInterval: 2000,
         submissionRetryCount: 1,
         batchSize: 20,
         awsConfig: {
-          accessKeyId: AccessKeyID,
-          secretAccessKey: SecretAccessKey,
-          region: "eu-west-2"
+          accessKeyId: accessKeyID,
+          secretAccessKey: secretAccessKey,
+          region: region
         },
         formatLog: item =>
           `${item.level}: ${item.message} ${JSON.stringify(item.meta)}`
@@ -61,8 +37,6 @@ const logger = createLogger({
   ]
 });
 
-//logger.add(new transports.File(level));
-
 process.on("uncaughtException", ex => {
   logger.error(ex.message);
 });
@@ -70,11 +44,5 @@ process.on("uncaughtException", ex => {
 process.on("unhandledRejection", ex => {
   logger.error(ex.message);
 });
-
-logger.stream = {
-  write: function(message, encoding) {
-    logger.debug(message);
-  }
-};
 
 module.exports = logger;
